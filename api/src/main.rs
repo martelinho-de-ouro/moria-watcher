c355b205a52443b9dfbbbad2e80b839ef6ef7849
Helio Frota <00hf11@gmail.com>
2024-07-01 08:33:44 -0300

    format

--- api/src/main.rs
+++ api/src/main.rs
@@ -28,7 +28,9 @@ async fn main() -> std::io::Result<()> {
 
     if app_env == AppEnv::Dev {
         match dotenvy::dotenv() {
-            Ok(path) => println!(".env read successfully from {}", path.display()),
+            Ok(path) => {
+                println!(".env read successfully from {}", path.display())
+            }
             Err(e) => println!("Could not load .env file: {e}"),
         };
     }
